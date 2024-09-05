c641458800ed47f9d46e5e55b56279ed4f193b72
Nathan <nldblanch@gmail.com>
2024-09-05 12:24:03 +0100

    create base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <Highlights />
+  </main>;
 };
 
 export default App;
