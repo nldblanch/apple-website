42a505c95b170ac39751672e3588dabec6919597
Nathan <nldblanch@gmail.com>
2024-09-05 16:44:04 +0100

    add canvas for 3d models

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
+import Model from "./components/Model";
 
 const App = () => {
   return <main className="bg-black">
     <Navbar />
     <Hero />
     <Highlights />
+    <Model />
   </main>;
 };
 
