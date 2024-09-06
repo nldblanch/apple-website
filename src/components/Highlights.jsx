bb31d74b6f73970f77cf38d837110ebaffdfd388
Nathan <nldblanch@gmail.com>
2024-09-06 16:11:44 +0100

    stagger animations

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -3,13 +3,32 @@ import gsap from "gsap";
 import React from "react";
 import { rightImg, watchImg } from "../utils";
 import VideoCarousel from "./VideoCarousel";
+import { animateWithGsap } from "../utils/animations";
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title", {
-      opacity: 1,
-      y: 0,
-    });
-    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+
+
+    animateWithGsap(
+      "#title",
+      { opacity: 1, y: 0, duration: 1, ease: "power1.inOut" },
+      {
+        start: "20% bottom",
+      }
+    );
+    animateWithGsap(
+      "#link1",
+      { opacity: 1, y: 0, duration: 1, ease: "power1.inOut" },
+      {
+        start: "80% bottom",
+      }
+    );
+    animateWithGsap(
+      "#link2",
+      { opacity: 1, y: 0, duration: 1, ease: "power1.inOut" },
+      {
+        start: "150% bottom",
+      }
+    );
   }, []);
   return (
     <section
@@ -22,11 +41,11 @@ const Highlights = () => {
             Get the highlights.
           </h1>
           <div className="flex flex-wrap items-end gap-5">
-            <p className="link">
+            <p id="link1" className="link">
               Watch the film
               <img src={watchImg} alt="watch" className="ml-2" />
             </p>
-            <p className="link">
+            <p id="link2" className="link">
               Watch the event
               <img src={rightImg} alt="right" className="ml-2" />
             </p>
