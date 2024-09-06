5c79bfae4811c639fe3a419f3cfb4ce2999aa1cf
Nathan <nldblanch@gmail.com>
2024-09-06 15:25:51 +0100

    add chip section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Model from "./components/Model";
 
 import * as Sentry from "@sentry/react";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 const App = () => {
   return (
     <main className="bg-black">
@@ -13,6 +14,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
